66ae47c1e9c3285fce2cb3fbd6540db347cfff94
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 13:51:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     # The license can be anything you like
     license='UD',
     description='Python package for analyzing human kinematic data, tailored for the Cashaback Lab',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
