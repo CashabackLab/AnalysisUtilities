82842ec88dd3288051bb20208ae7ca0151c5bb3a
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 14:35:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['analysis_utilities'],
     # Needed for dependencies
-    install_requires=['numpy', 'numba', 'scipy'],
+    install_requires=['numpy', 'numba', 'scipy', 'tqdm'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
