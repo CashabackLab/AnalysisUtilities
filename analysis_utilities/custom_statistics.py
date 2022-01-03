6532ee83e7bdde34f7e068862960c4c0af09a65d
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 14:35:48 -0500

    Update custom_statistics.py

--- analysis_utilities/custom_statistics.py
+++ analysis_utilities/custom_statistics.py
@@ -33,6 +33,12 @@ def Two_Tailed_Bootstrap(data1, data2, M = 1e4, paired = False, verbose = False)
             results[i] = mean_diff
     
     centered = results - original_mean_diff
+    
+    def varName(p):
+        "Return name of the variable as a string"
+        for k, v in globals().items():
+            if id(p) == id(v):
+            return k
     p_val = np.sum(centered > abs(original_mean_diff)) + np.sum(centered < -abs(original_mean_diff))
     p_val /= M
     
