a5ef7b9c7e38bfc509b5238f0499b494da592e0c
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 13:36:28 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings 
+    name='AnalysisUtilities',
+    url='https://github.com/CashabackLab/AnalysisUtilities',
+    author='CashabackLab',
+    author_email='cashabacklab@gmail.com',
+    # Needed to actually package something
+    packages=['statistics'],
+    # Needed for dependencies
+    install_requires=['numpy', 'numba', 'scipy', 'warnings'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='UD',
+    description='Python package for analyzing human kinematic data, tailored for the Cashaback Lab',
+    long_description=open('README.txt').read(),
+)
