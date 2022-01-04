7d739b57ecd4ab271c4c6ac713b4b75fa605ac6a
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-04 08:35:56 -0500

    Update __init__.py

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -1,8 +1,1 @@
-def varName(p):
-    """
-    Return name of the variable as a string
-    p: variable of any type
-    """
-    for k, v in globals().items():
-        if id(p) == id(v):
-            return k
+from . import custom_statistics
