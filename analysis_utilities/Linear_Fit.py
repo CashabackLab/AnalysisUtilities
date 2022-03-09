6bd8875dbff2051adbefa1eaaa821f7fd1829367
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-03-09 15:20:24 -0500

    Create Linear_Fit.py

---
+++ analysis_utilities/Linear_Fit.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def Linear_Fit(array_A, array_B):
+    """
+    Returns slope and y-intercept of the line of best fit
+    """
+    array_A = np.array(array_A)
+    array_B = np.array(array_B)
+    
+    #Pair arrays then sort them for easier fit
+    zipped_list = zip(array_A[~np.isnan(array_A)], array_B[~np.isnan(array_B)])
+    sorted_list = sorted(zipped_list)
+    sorted_a, sorted_b = zip(*sorted_list)
+    
+    m, b = np.polyfit(sorted_a, sorted_b, 1)
+    return m, b
