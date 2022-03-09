aac5ef466641240ca840923a63d6bbb8c6fb6f6e
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-03-09 15:23:29 -0500

    Update __init__.py

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -1,3 +1,9 @@
 #allows using functions like analysis_utilities.custom_statistics.FunctionName()
 #from . import custom_statistics
-from.custom_statistics import *
+# from.custom_statistics import *
+
+from .Bootstrap import *
+from .Grubbs import *
+from .Linear_fit import *
+from .CLES import *
+from .Cohen_D import *
