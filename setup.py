86145b0da13693ed2640f36c17fff54dfa3b23e7
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-08-02 12:41:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'numba', 'scipy', 'tqdm'],
     # *strongly* suggested for sharing
-    version='0.3.4',
+    version='0.3.5',
     # The license can be anything you like
     license='MIT',
     description='Python package for analyzing human kinematic data, tailored for the Cashaback Lab',
