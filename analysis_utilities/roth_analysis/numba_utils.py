fb10c268ef41a29096dc23174eda893cad4ff1fb
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-08-17 15:12:34 -0400

    Create numba_utils.py

---
+++ analysis_utilities/roth_analysis/numba_utils.py
@@ -0,0 +1,28 @@
+from numba import njit
+import numpy as np
+
+@njit
+def np_apply_along_axis(func1d, axis, arr):
+    assert arr.ndim == 2
+    assert axis in [0, 1]
+    if axis == 0:
+        result = np.empty(arr.shape[1])
+        for i in range(len(result)):
+            result[i] = func1d(arr[:, i])
+    else:
+        result = np.empty(arr.shape[0])
+        for i in range(len(result)):
+            result[i] = func1d(arr[i, :])
+    return result
+
+@njit
+def nb_nanstd(array, axis):
+    return np_apply_along_axis(np.nanstd, axis, array)
+
+@njit
+def nb_mean(array, axis):
+    return np_apply_along_axis(np.mean, axis, array)
+
+@njit
+def nb_nanmean(array, axis):
+    return np_apply_along_axis(np.nanmean, axis, array)
