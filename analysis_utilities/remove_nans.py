50f11fef7d8afa39d1273ae5f2c7bf00eaaaef7e
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-08-17 11:25:39 -0400

    can optionally maintain original dimensions

--- analysis_utilities/remove_nans.py
+++ analysis_utilities/remove_nans.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-def remove_nans(array):
+def remove_nans(array, replace_value = None):
     """
     Removes nan values from a numpy array or list
-    Returns a flattened array with all nans removed
+    Returns a flattened array with all nans removed by default
+    set replace_value to a value to maintain original dimensions and replace nans with the specified value
     """
     array = np.array(array)
-    clean_array = array[~np.isnan(array)]
-    
+    if replace_value == None:
+        clean_array = array[~np.isnan(array)]
+    else:
+        clean_array = np.where(np.isfinite(array), array, replace_value)
+
     return clean_array
