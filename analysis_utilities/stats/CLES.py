49ae289147bb2430445d841ff1cc2662d5f37918
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-11-07 10:56:58 -0500

    added new cles function
    New cles function has keywords that are in line with the Bootstrap keywords for easier compatability

--- analysis_utilities/stats/CLES.py
+++ analysis_utilities/stats/CLES.py
@@ -1,4 +1,5 @@
 import numpy as np
+import warnings
 
 def _Common_Language_EF_One_Sample(data, mu):
     total = 0
@@ -24,6 +25,7 @@ def _Common_Language_EF_Two_Sample(data1, data2):
     return total / (len(data1) * len(data2))
 
 def CLES(data1, data2, paired = False, sample = "two sample", alt_comparison = 0):
+    warnings.warn("Outdated function, please use \cles\ for better compatability.")
     """
     Computes the common language effect size for the given data
     if sample = "one sample", data2 should be treated as a single number, mu, the mean to compare the data to
@@ -38,3 +40,40 @@ def CLES(data1, data2, paired = False, sample = "two sample", alt_comparison = 0
     elif sample.lower() == "paired" or paired == True:
         data = np.array(data1) - np.array(data2)
         return _Common_Language_EF_One_Sample(data, alt_comparison) *100
+    
+def cles(data1, data2 = 0, paired = False, alternative = "greater", **kwargs):
+    """
+    Computes the common language effect size for the given data.
+    If only one data array is given, computes one sample cles
+    If only one data array is given and second input is a number, 
+        computes cles comparison to the given number
+    alternative : {greater, less}; default is greater
+    paired: {True, False}, must give data array as second input
+    
+    """
+    array_flag = 0
+    if type(data2) == type(list()) or type(data2) == type(np.array(1)):
+        array_flag = 1
+        
+    ##if two samples given and not paired
+    if alternative == "greater" and paired == False and array_flag:
+        theta = _Common_Language_EF_Two_Sample(data1, data2)
+        
+    elif alternative == "less" and paired == False and array_flag:
+        theta = _Common_Language_EF_Two_Sample(data2, data1)
+        
+    ##if one sample is given and second sample is value to compare to
+    elif alternative == "greater" and paired == False and not array_flag:
+        theta = _Common_Language_EF_Two_Sample(data1, data2)
+        
+    elif alternative == "less" and paired == False and not array_flag:
+        theta = _Common_Language_EF_Two_Sample(data2, data1)
+    
+    ## if two samples are given and they are paired
+    elif alternative == "greater" and paired == True:
+        theta = _Common_Language_EF_One_Sample(data1 - data2, 0)
+        
+    elif alternative == "less" and paired == True:
+        theta = _Common_Language_EF_One_Sample(data2 - data1, 0)
+        
+    return theta * 100
