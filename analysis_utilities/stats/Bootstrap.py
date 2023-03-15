e54b46da859d8425be3c833142748f98e4393be9
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-03-15 15:26:56 -0400

    added a lowercase bootstrap that can do either means or medians

--- analysis_utilities/stats/Bootstrap.py
+++ analysis_utilities/stats/Bootstrap.py
@@ -96,3 +96,99 @@ def Bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided", 
         return p_val, distribution
     else:
         return p_val
+    
+@njit(parallel = True)
+def _nb_median_bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided"):
+    rng = np.random
+
+    results = np.empty(M) * np.nan
+    original_mean_diff = np.nanmedian(data1) - np.nanmedian(data2)
+
+    if paired:
+        assert data1.shape == data2.shape
+        
+        #Want to resample from the distribution of paired differences with replacement
+        paired_diff = data1 - data2
+        data_len = paired_diff.shape[0]
+        
+        for i in nb.prange(M):
+            results[i] = np.nanmedian(rng.choice(paired_diff, size = data_len, replace = True))
+    else:        
+        data_len = data1.shape[0]
+        
+        #create a bucket with all data thrown inside
+        pooled_data = np.empty(data1.shape[0] + data2.shape[0]) * np.nan
+        pooled_data[:data1.shape[0]] = data1
+        pooled_data[data1.shape[0]:] = data2
+        
+        #Recreate the two groups by sampling without replacement
+        for i in nb.prange(M): 
+            tmp = rng.choice(pooled_data, size = len(pooled_data), replace = False)
+            data1_resample = tmp[:data_len] #up to number of points in data1
+            data2_resample = tmp[data_len:] #the rest are in data2
+            mean_diff = np.nanmedian(data1_resample) - np.nanmedian(data2_resample)
+            results[i] = mean_diff
+        
+    #center the results on 0
+    centered_results = results - np.nanmean(results)
+
+    if alternative == "two-sided":
+        #are the results more extreme than the original?
+        p_val = np.sum(centered_results > abs(original_mean_diff)) + np.sum(centered_results < -abs(original_mean_diff))
+        returned_distribution = centered_results
+    elif alternative == "greater":
+        #are results greater than the original?
+        p_val = np.sum(centered_results - (original_mean_diff) > 0)
+        returned_distribution = centered_results - abs(original_mean_diff)
+    elif alternative == "less":
+        #are results less than the original?
+        p_val = np.sum(centered_results + (original_mean_diff) > 0)
+        returned_distribution = centered_results + abs(original_mean_diff)
+    else:
+        raise ValueError("alternative must be \"two-sided\", \"greater\", or \"less\"")
+        
+    return p_val / M, returned_distribution
+
+def bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided", return_distribution = False, test = "mean", **kwargs):
+    """ 
+    Bootstrap difference in means or medians between two groups.
+    M = float64 # Number of iterations
+    paired = {True, False}
+    alternative = {"two-sided", "greater", "less"} #data1 relative to data2, i.e.: data1 "greater" than data2
+    return_distribution {True, False} #returns the bootstrapped distribution
+    test = {"mean", "median"} #compares either differences in means or median
+    
+    ##Legacy code for backwards compatability. Do not reccomend usage.
+    direction = {"greater", "lesser", None} 
+    if verbose = True, returns p_value and distribution
+    if not verbose, only returns p-value
+    """
+    #BackCompat keywords
+    direction = kwargs.get("direction", None)
+    verbose = kwargs.get("verbose", False)
+    
+    #make sure data is in np.array format
+    data1, data2 = np.array(data1), np.array(data2)
+    
+    #make M an integer
+    M = int(M)
+    
+    #handle backwards compatability
+    if direction == "greater": 
+        alternative = direction
+    elif direction == "lesser": 
+        alternative = "less"
+    elif direction != None: 
+        raise ValueError("\"direction\" keyword misused. Please use the \"alternative\" keyward argument.")
+    
+    if test == "mean":
+        p_val, distribution = _nb_mean_bootstrap(data1, data2, M = M, paired = paired, alternative = alternative)
+    elif test == "median":
+        p_val, distribution = _nb_median_bootstrap(data1, data2, M = M, paired = paired, alternative = alternative)
+    else:
+        raise ValueError("\"test\" must be \"mean\" or \"median\".")
+        
+    if verbose or return_distribution:
+        return p_val, distribution
+    else:
+        return p_val
