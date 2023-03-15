428696e85a565a9f0b1df57fa457a9e6c031ea50
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-03-15 16:02:39 -0400

    Update Bootstrap.py

--- analysis_utilities/stats/Bootstrap.py
+++ analysis_utilities/stats/Bootstrap.py
@@ -98,6 +98,58 @@ def Bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided", 
         return p_val
     
 @njit(parallel = True)
+def _nb_mean_bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided"):
+    rng = np.random
+
+    results = np.empty(M) * np.nan
+    original_mean_diff = np.nanmean(data1) - np.nanmean(data2)
+
+    if paired:
+        assert data1.shape == data2.shape
+        
+        #Want to resample from the distribution of paired differences with replacement
+        paired_diff = data1 - data2
+        data_len = paired_diff.shape[0]
+        
+        for i in nb.prange(M):
+            results[i] = np.nanmean(rng.choice(paired_diff, size = data_len, replace = True))
+    else:        
+        data_len = data1.shape[0]
+        
+        #create a bucket with all data thrown inside
+        pooled_data = np.empty(data1.shape[0] + data2.shape[0]) * np.nan
+        pooled_data[:data1.shape[0]] = data1
+        pooled_data[data1.shape[0]:] = data2
+        
+        #Recreate the two groups by sampling without replacement
+        for i in nb.prange(M): 
+            tmp = rng.choice(pooled_data, size = len(pooled_data), replace = False)
+            data1_resample = tmp[:data_len] #up to number of points in data1
+            data2_resample = tmp[data_len:] #the rest are in data2
+            mean_diff = np.nanmean(data1_resample) - np.nanmean(data2_resample)
+            results[i] = mean_diff
+        
+    #center the results on 0
+    centered_results = results - np.nanmean(results)
+
+    if alternative == "two-sided":
+        #are the results more extreme than the original?
+        p_val = np.sum(centered_results > abs(original_mean_diff)) + np.sum(centered_results < -abs(original_mean_diff))
+        returned_distribution = centered_results
+    elif alternative == "greater":
+        #are results greater than the original?
+        p_val = np.sum(centered_results - (original_mean_diff) > 0)
+        returned_distribution = centered_results - abs(original_mean_diff)
+    elif alternative == "less":
+        #are results less than the original?
+        p_val = np.sum(centered_results + (original_mean_diff) > 0)
+        returned_distribution = centered_results + abs(original_mean_diff)
+    else:
+        raise ValueError("alternative must be \"two-sided\", \"greater\", or \"less\"")
+        
+    return p_val / M, returned_distribution
+
+@njit(parallel = True)
 def _nb_median_bootstrap(data1, data2, M = 1e4, paired = False, alternative = "two-sided"):
     rng = np.random
 
