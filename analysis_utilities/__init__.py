1b8388418e6d0979e47606cee010ac27084e5805
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-04-17 11:00:49 -0400

    Update __init__.py

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -2,7 +2,7 @@
 #from . import custom_statistics
 # from.custom_statistics import *
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 
 from .stats.Bootstrap import Bootstrap, bootstrap
 from .stats.Grubbs import Grubbs
@@ -12,5 +12,3 @@ from .stats.holmbonferroni_correction import holmbonferroni_correction
 
 from .Linear_Fit import Linear_Fit
 from .remove_nans import remove_nans
-
-#import .roth_utilities
