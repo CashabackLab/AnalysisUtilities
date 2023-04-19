3bac31e574b26fb079386725a3ccc1cf3b3e9231
JanCalalo <97128826+JanCalalo@users.noreply.github.com>
2023-04-19 13:49:37 -0400

    Update __init__.py

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -2,7 +2,7 @@
 #from . import custom_statistics
 # from.custom_statistics import *
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 
 from .stats.Bootstrap import Bootstrap, bootstrap
 from .stats.Grubbs import Grubbs
