01c44e414cf4ade60e9f0ab0e795bfa2d8dd3a1b
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2023-11-17 15:49:09 -0500

    Update __init__.py

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -2,7 +2,7 @@
 #from . import custom_statistics
 # from.custom_statistics import *
 
-__version__ = "0.5.12"
+__version__ = "0.5.13"
 
 from .stats.Bootstrap import Bootstrap, bootstrap
 from .stats.Grubbs import Grubbs
