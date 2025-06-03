b41144c58eca9aa31429cc1050e7e6fe7f515d5f
SethSullivan <sethsull@udel.edu>
2025-06-03 12:04:51 -0400

    got rid of example test

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -6,13 +6,7 @@ def test_nb_bootstrap():
     fake_data = 1
     test_data = 1
     # assert fake_data != test_data
-
-def test_add():
-    my_output = 5
-    function_output = bootstrap(2,3)
-    assert my_output == function_output
     
 if __name__ == "__main__":
-    # test_nb_bootstrap()
-    test_add()
+    test_nb_bootstrap()
     
