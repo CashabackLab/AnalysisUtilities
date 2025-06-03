3daf8065ffc06d6aa6bee334f72ec2e1cc24f671
SethSullivan <sethsull@udel.edu>
2025-06-03 09:53:08 -0400

    added nanmedian and updated np_apply_along_axis to take a 1d array

--- analysis_utilities/roth_analysis/numba_utils.py
+++ analysis_utilities/roth_analysis/numba_utils.py
@@ -1,28 +1,35 @@
 from numba import njit
 import numpy as np
 
-@njit
-def np_apply_along_axis(func1d, axis, arr):
-    assert arr.ndim == 2
+@nb.njit
+def np_apply_along_axis(func1d, arr, axis):
     assert axis in [0, 1]
-    if axis == 0:
-        result = np.empty(arr.shape[1])
-        for i in range(len(result)):
-            result[i] = func1d(arr[:, i])
-    else:
-        result = np.empty(arr.shape[0])
-        for i in range(len(result)):
-            result[i] = func1d(arr[i, :])
+
+    if arr.ndim == 1:
+        result = func1d(arr)
+    else:            
+        if axis == 0:
+            result = np.empty(arr.shape[1])
+            for i in range(len(result)):
+                result[i] = func1d(arr[:, i])
+        else:
+            result = np.empty(arr.shape[0])
+            for i in range(len(result)):
+                result[i] = func1d(arr[i, :])
     return result
 
 @njit
 def nb_nanstd(array, axis):
-    return np_apply_along_axis(np.nanstd, axis, array)
+    return np_apply_along_axis(np.nanstd, array, axis)
 
 @njit
 def nb_mean(array, axis):
-    return np_apply_along_axis(np.mean, axis, array)
+    return np_apply_along_axis(np.mean, array, axis)
 
 @njit
 def nb_nanmean(array, axis):
-    return np_apply_along_axis(np.nanmean, axis, array)
+    return np_apply_along_axis(np.nanmean, array, axis)
+
+@nb.njit
+def nb_nanmedian(array, axis):
+    return np_apply_along_axis(np.nanmedian, array, axis)
