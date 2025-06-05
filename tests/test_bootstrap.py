e08e4ff56df075e091cecbdb1c3a4acf05873174
SethSullivan <sethsull@udel.edu>
2025-06-05 12:18:46 -0400

    formatting and leaving unpaired tests as True for now

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -22,6 +22,9 @@ def test_nb_bootstrap_edgecases_paired():
     
         assert out == expected_pval[i], f"{i} didn't work"
 
+def test_nb_boostrap_unpaired():
+    assert True
+
 def test_nb_boostrap_against_pingouin():
     
    data1 = np.random.normal(0,10,1000) 
@@ -42,6 +45,7 @@ def test_nb_boostrap_against_pingouin():
                 assert np.abs(ttest_pval - boot_pval) < 0.01
             
 def test_nb_bootstrap_josh_boot_example():
+    CORRECT_OUTPUT = 0.0262
     g_control = [87, 90, 82, 77, 71, 81, 77, 79, 84, 86, 78, 84, 86, 69, 81, 75, 70, 76, 75, 93]
     g_drug = [74, 67, 81, 61, 64, 75, 81, 81, 81, 67, 72, 78, 83, 85, 56, 78, 77, 80, 79, 74]
     boot_pval = bootstrap(g_control, g_drug, 
@@ -51,10 +55,6 @@ def test_nb_bootstrap_josh_boot_example():
                                 stat_type = "mean", 
                                 return_distribution = False, 
                                 seed=20)
-    print(boot_pval)
-    assert np.abs(boot_pval - 0.0262) < 0.005 
+    assert np.abs(boot_pval - CORRECT_OUTPUT) < 0.005 
 
-
-def test_nb_boostrap_unpaired():
-    pass
     
