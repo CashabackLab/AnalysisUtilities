511407afd27eac054ed15d13c7a6316573083b38
JBuggeln <100240250+JohnBuggeln@users.noreply.github.com>
2025-07-18 11:57:30 -0400

    Update __init__.py version

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -2,7 +2,7 @@
 #from . import custom_statistics
 # from.custom_statistics import *
 
-__version__ = "0.6.20"
+__version__ = "0.6.21"
 
 from .stats.Bootstrap import bootstrap
 from .stats.Grubbs import grubbs
