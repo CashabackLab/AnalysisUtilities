6c25288af63ff18c0976693f524c3ee415e23b80
Truc Ngo <tructn@udel.edu>
2026-04-18 12:09:20 -0400

    Revert "Bump version to 0.6.26"
    This reverts commit 88c99397f2e1965ff4e4aa1464ed12af2c0a2c01.

--- analysis_utilities/__init__.py
+++ analysis_utilities/__init__.py
@@ -2,7 +2,7 @@
 #from . import custom_statistics
 # from.custom_statistics import *
 
-__version__ = "0.6.26"
+__version__ = "0.6.25"
 
 from .stats.Bootstrap import compare_to_null
 from .stats.Bootstrap import linear_regression_func
